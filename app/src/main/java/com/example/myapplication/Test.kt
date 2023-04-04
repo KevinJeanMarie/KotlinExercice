0a89de95132e3ab74776c719071e36d813c81595
KevinJeanMarie <kjeanmarie01@gmail.com>
2023-04-04 10:56:58 +0200

    ex 5 part 2

--- app/src/main/java/com/example/myapplication/Test.kt
+++ app/src/main/java/com/example/myapplication/Test.kt
@@ -1,5 +1,73 @@
 fun main() {
+    val w = Weather(10f, "Paris")
+    val w1 = object : Weather(10f, "Paris") {
 
+        override fun isHot() = temp > 10f
+
+    }
+
+    object : Listener {
+        override fun onClicked() {
+            TODO("Not yet implemented")
+        }
+
+    }
+}
+
+interface Listener {
+
+    fun onClicked()
+
+}
+
+
+open class Weather(val temp: Float, val city: String) {
+
+    companion object {
+        const val MIN_TEMP = 0f
+        const val MAX_TEMP = 25f
+    }
+
+
+    constructor(weather: Weather) : this(
+        weather.temp,
+        weather.city,
+    );
+
+    override fun toString(): String {
+        return "Température de $city : $temp"
+    }
+
+    fun isCold() = temp < MIN_TEMP
+
+    open fun isHot() = temp > MAX_TEMP
+
+}
+
+class LocalWeather(temp: Float, city: String) : Weather(temp, city) {
+
+    companion object {
+        const val MAX_TEMP = 25f
+    }
+
+
+    override fun isHot() = temp > 32f
+
+}
+
+sealed interface AppState
+object AppStateLoading
+data class AppStateSuccess(val value: String) : AppState
+data class AppStateError(val value: String) : AppState
+
+
+
+
+
+
+
+/*
+fun main() {
     val str: String = "hello"
     print("la string est $str")
 
@@ -10,27 +78,28 @@ fun main() {
     //val attr : String? = null
     val attr : String = "ceci est une string"
     print(attr.uppercase())
-
-    print(isEven)
 }
 
-fun isEven(number: Int) = 2 == 0
-print(IsEven)
-fun isEven(number: Int): Boolean return number % 2 == 0 ? true : false
+//fun isEven(number: Int) = 2 == 0
+//fun isEven(number: Int): Boolean return number % 2 == 0 ? true : false
 
 /*
 fun Int.isEven() {
     return this % 2 == 0
 }
 */
-/*fun isEven(number: Int): Boolean {
-    if (number % 2 == 0) {
-        (return true)
-    } else {
-        (return false)
-    }
-    print(isEven(4))
- */
+fun isEven(number: Int): Boolean {
+    return number % 2 == 0
+}
+
+class User(val firstName: String, val lastName: String){
+    val user1 = User("JM", "Kevin")
+    val user2 = User("JM", "Kevin")
+
+    return (user1 == user2)
+
+}
+
 
 /*   var attr : String? = null
     attr = ""
@@ -51,3 +120,5 @@ fun Int.isEven() {
 //syntax différente mais même resultat
 //var attr : String = "Hello"
 //val attr = "Hello"
+
+ */
